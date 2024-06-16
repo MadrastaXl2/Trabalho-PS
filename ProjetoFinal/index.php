e203ea51c47c62bcdb7b76a6dcab5aa8417d4a25
MadrastaXl2 <147931416+MadrastaXl2@users.noreply.github.com>
2024-06-16 20:42:24 -0300

    .

--- ProjetoFinal/index.php
+++ ProjetoFinal/index.php
@@ -14,156 +14,34 @@
 </head>
 
 <body>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    <!-- Inicio Cabeçalho-->
+    <header id="cabecalho">
+        <div id="imgLogo"> <!--Div da logo-->
+            <img src="IMGS/Logo.png" width="15%" alt="logo">
+            <h10 id="RemediosJa">Remédios Já</h10>
+        </div>
+        <div id="textos_do_meio">
+            <a href="">Sobre nós</a>
+            <a href="">O que fazemos</a>
+            <a href="">Fale conosco</a>
+        </div>
+        <div id="ParteFinalCabecalho">
+            <img id="tresListras" src="IMGS/menu.png" alt="Tres listras" width="60%">
+            <div class="dropdown">
+                <img src="IMGS/Usuario.png" type="button" data-bs-toggle="dropdown" aria-expanded="false" width="60%"
+                    alt="Usuário">
+                <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="#">Login</a></li>
+                    <li><a class="dropdown-item" href="#">Cadastro</a></li>
+                    <li><a class="dropdown-item" href="#">Home</a></li>
+                </ul>
+            </div>
+        </div>
+    </header>
+    <div>
+        <img src="IMGS/ImagemBanner.png" width="100%" alt="">
+
+    </div>
     <div id="sobreNos">
         <div id="titleSN">
             <h1>Sobre Nós</h1>
@@ -193,7 +71,8 @@
             </div>
             <div class="geralClient">
                 <div class="txtClient">
-                    <h4>“Desde que comecei a usar o serviço, não perdi nenhuma dose. A interface é extremamente amigável, o que torna toda a experiência muito agradável.“</h4>
+                    <h4>“Desde que comecei a usar o serviço, não perdi nenhuma dose. A interface é extremamente
+                        amigável, o que torna toda a experiência muito agradável.“</h4>
                     <p><strong>- Peter Parker</strong></p>
                 </div>
                 <img src="https://lojadosherois.com.br/cdn/shop/files/1077732_1685915288095_800x.jpg?v=1685915306"
@@ -201,6 +80,199 @@
             </div>
         </div>
     </div>
+</body>
+
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<div id="oqFazemos">
+    <div id="titleFaz">
+        <h1>O que Fazemos</h1>
+    </div>
+    <div id="lineFaz"></div>
+</div>
+
+
+
+<div id="sobreNos">
+    <div id="titleSN">
+        <h1>Sobre Nós</h1>
+    </div>
+    <div id="lineSN"></div>
+    <div id="txtSN">
+        <h3>Na <strong>Remédios Já!!!</strong>, estamos comprometidos com sua saúde. Sabemos que a aderência ao
+            tratamento é crucial, e estamos aqui para tornar essa tarefa o mais simples e eficiente possível.
+            Junte-se a nós e descubra como podemos transformar sua rotina de medicação em uma experiência mais
+            tranquila e eficaz.</h5>
+    </div>
+</div>
+<div id="avaliacoes">
+    <div id="titleAvaliacoes">
+        <h1>Avaliações dos clientes</h1>
+        <div id="lineAvaliacoes"></div>
+    </div>
+    <div id="customerRatings">
+        <div class="geralClient">
+            <div class="txtClient">
+                <h4>“Estou completamente encantada com o site Remedios Já! Desde que comecei a usar, minha vida
+                    melhorou significativamente!“</h4>
+                <p><strong>- Juliana Bianchely</strong></p>
+            </div>
+            <img src="https://cdn.pixabay.com/photo/2014/09/03/03/54/beautiful-woman-434015_1280.jpg" alt="foto mulher">
+        </div>
+        <div class="geralClient">
+            <div class="txtClient">
+                <h4>“Desde que comecei a usar o serviço, não perdi nenhuma dose. A interface é extremamente amigável, o
+                    que torna toda a experiência muito agradável.“</h4>
+                <p><strong>- Peter Parker</strong></p>
+            </div>
+            <img src="https://lojadosherois.com.br/cdn/shop/files/1077732_1685915288095_800x.jpg?v=1685915306"
+                alt="foto homem">
+        </div>
+    </div>
+</div>
 </body>
 
 </html>
