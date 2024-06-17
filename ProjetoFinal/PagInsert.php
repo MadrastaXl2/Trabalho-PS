63ef0c154d4753baea0bc7e4e67df2a84cc58077
podreiro <rafaelp3r3ira@gmail.com>
2024-06-16 21:10:50 -0300

    Insert
    dadad

--- ProjetoFinal/PagInsert.php
+++ ProjetoFinal/PagInsert.php
@@ -5,8 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <title>Página de Cadastro</title>
-    <link rel="stylesheet" href="cadastro.css">
+    <title>Página de Cadastro</title>  
+    <link rel="stylesheet" href="mainPage.css">
     <style type="text/css">
         body {
             margin: 20px;
@@ -15,9 +15,36 @@
 </head>
 
 <body>
+<header id = "cabecalho">
+        <div id = "imgLogo"> <!--Div da logo-->
+            <div id = "logo">
+                <img src="IMGS/Logo.png" width="30%"  alt="logo">
+                <h10 id = "RemediosJa">Remédios  Já !!!</h10>
+            </div>           
+        </div>
+        <div id = "textos_do_meio"> 
+                <div>| <a href=""> Sobre nós</a></div>
+                <div><a href="">O que fazemos</a></div>
+                <div><a href="">Fale conosco </a> |</div>
+        </div>
+        <div id = "ParteFinalCabecalho">
+            <div id = "ParteRedonda">
+                <img id ="tresListras"src="IMGS/menu.png" alt="Tres listras" width="50%">
+                <div class="dropdown">                
+                    <img src="IMGS/Usuario.png" type="button" data-bs-toggle="dropdown" aria-expanded="false" width="50%" alt="Usuário">                 
+                    <ul class="dropdown-menu">
+                      <li><a class="dropdown-item" href="">Login</a></li>
+                      <li><a class="dropdown-item" href="PagInsert.php">Cadastro</a></li>
+                      <li><a class="dropdown-item" href="#">Home</a></li>
+                    </ul>
+                  </div>
+            </div>
+        </div>
+
+    </header>
     <div id="divGeral">
         <h1 class="text-center">Cadastro Usuario</h1>
-        <form method="POST" action="insert_cadastro.php">
+        <form method="POST" action="PhpInsert.php">
             <div class="mb-3">
                 <label for="exampleFormControlInput1" class="form-label">Nome: </label>
                 <input type="text" class="form-control" id="exampleFormControlInput1" name="nome">
@@ -48,6 +75,8 @@
             <div id="buttons" class="text-center">
                 <button type="submit" name="submit" class="btn btn-success">Cadastrar</button>
             </div>
+            
+            
             </form>
     </div>
 </body>
