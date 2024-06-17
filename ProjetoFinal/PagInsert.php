9e5051049aba01cd85f884206438759d87a04964
podreiro <rafaelp3r3ira@gmail.com>
2024-06-17 20:37:53 -0300

    Commit pag de login
    Nesse commit fiz a pag de login toda php,html,css. Coloquei um btn no insert e mudei a lingua do index para pt br

--- ProjetoFinal/PagInsert.php
+++ ProjetoFinal/PagInsert.php
@@ -6,11 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <title>Página de Cadastro</title>  
-    <link rel="stylesheet" href="mainPage.css">
+    <link rel="stylesheet" href="MainPage.css">
     <style type="text/css">
-        body {
-            margin: 20px;
-        }
     </style>
 </head>
 
@@ -73,8 +70,14 @@
             </div>
 
             <div id="buttons" class="text-center">
-                <button type="submit" name="submit" class="btn btn-success">Cadastrar</button>
+                <div>
+                    <input type="submit" name="submit" class="btn btn-success" value = "Cadastrar">
+                </div>               
+                <div id = "btnVoltar">
+                    <a  href="index.php">Voltar</a>
+                </div>                
             </div>
+
             
             
             </form>
