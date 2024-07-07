0dcd8650cffc103097380d066f5219da42117716
MadrastaXl2 <147931416+MadrastaXl2@users.noreply.github.com>
2024-07-07 17:26:21 -0300

    Adicionei o atualizaRemedio na main

--- ProjetoFinal/PAGS/faleConosco.php
+++ ProjetoFinal/PAGS/faleConosco.php
@@ -1,15 +1,13 @@
 <!DOCTYPE html>
 
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
-        crossorigin="anonymous"></script>
-        <link rel="stylesheet" href="./CSS/MainPage.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="./CSS/MainPage.css">
     <title>Remédios Já</title>
 </head>
 
@@ -28,8 +26,7 @@
         <div id="ParteFinalCabecalho">
             <img id="tresListras" src="./IMGS/menu.png" alt="Tres listras" width="60%">
             <div class="dropdown">
-                <img src="./IMGS/Usuario.png" type="button" data-bs-toggle="dropdown" aria-expanded="false" width="60%"
-                    alt="Usuário">
+                <img src="./IMGS/Usuario.png" type="button" data-bs-toggle="dropdown" aria-expanded="false" width="60%" alt="Usuário">
                 <ul class="dropdown-menu">
                     <li><a class="dropdown-item" href="./PAGS/LoginHtml.php">Login</a></li>
                     <li><a class="dropdown-item" href="./PAGS/PagInsert.php">Cadastro</a></li>
@@ -37,5 +34,24 @@
                 </ul>
             </div>
         </div>
+        <form id="form" method="post" action="envia.php">
+            <div class="form-floating mb-3">
+                <input type="email" class="form-control" id="floatingInput" placeholder="Nome">
+                <label for="floatingInput">Escreva o seu nome</label>
+            </div>
+            <div class="form-floating">
+                <input type="password" class="form-control" id="floatingPassword" placeholder="Email">
+                <label for="floatingPassword">Escreva o seu Email</label>
+            </div>
+            <div class="form-floating">
+                <input type="password" class="form-control" id="floatingPassword" placeholder="Assunto">
+                <label for="floatingPassword">Escreva o assunto do Email</label>
+            </div>
+            <div class="form-floating">
+                <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea2" style="height: 100px"></textarea>
+                <label for="floatingTextarea2">Escreva o que você deseja falar</label>
+            </div>
+            input
+        </form>
     </header>
 </body>
---
+++ ProjetoFinal/PHP/atualizaRemedio.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>Atualizar Remédio</title>
+</head>
+
+<body>
+    <?php
+    // Dados de conexão
+    $hostname = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "pit";
+    $resultado = false;
+
+    // Conectar
+    $conn = new mysqli($hostname, $username, $password, $database);
+
+    // Verificar conexão
+    if ($conn->connect_error) {
+        die("<div class='alert alert-danger' role='alert'>Erro ao conectar: " . $conn->connect_error . "</div>");
+    }
+
+    // Verificar se o ID está definido
+    if (isset($_GET['id'])) {
+        $id = $conn->real_escape_string($_GET['id']);
+        // Criar o comando
+        $sql = "SELECT * FROM `remedio` WHERE id = '$id'";
+        // Executar o comando
+        $resultadoSelect = $conn->query($sql);
+        if (!$resultadoSelect) {
+            echo "<div class='alert alert-danger' role='alert'>Erro ao processar</div>";
+        }
+    }
+
+    // Verificar se os campos POST não estão vazios
+    if (!empty($_POST['id']) && !empty($_POST['Nome']) && !empty($_POST['Horario']) && !empty($_POST['Data'])) {
+        $id = $conn->real_escape_string($_POST['id']);
+        $nome = $conn->real_escape_string($_POST['Nome']);
+        $horario = $conn->real_escape_string($_POST['Horario']);
+        $data = $conn->real_escape_string($_POST['Data']);
+        // Criar a consulta SQL de atualização
+        $sql = "UPDATE `registro` SET Nome = '$nome', Horario = '$horario', Data = '$data' WHERE id = '$id'";
+        // Executar a consulta
+        if ($conn->query($sql) === TRUE) {
+            $resultado = true;
+        } else {
+            echo "<div class='alert alert-danger' role='alert'>Erro ao alterar os dados: " . $conn->error . "</div>";
+        }
+    }
+    // Fechar conexão
+    $conn->close();
+    ?>
+
+    <?php if ($resultado) : ?>
+        <div class="alert alert-success" role="alert">
+            Dado alterado com sucesso!
+        </div>
+    <?php else : ?>
+        <div class="alert alert-danger" role="alert">
+            Erro ao alterar o dado!
+        </div>
+    <?php endif ?>
+
+    <a href="verRemedios.php" class="btn btn-primary">Ver dados</a>
+</body>
+
+</html>
