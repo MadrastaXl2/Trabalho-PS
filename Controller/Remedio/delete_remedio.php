0b8fc6ac8fcd5a4117f32b9b1729dfa1d92085c1
podreiro <125074659+podreiro@users.noreply.github.com>
2024-11-12 14:34:53 -0300

    Delete_Remedio

--- Controller/Remedio/delete_remedio.php
+++ Controller/Remedio/delete_remedio.php
@@ -1,15 +1,33 @@
 <?php 
 
-include '.../Model/MenagerRemedio.class.php';
-include '.../Model/Conexao.class.php';
+require_once("../../Model/ManagerRemedio.class.php");
+require_once("../../Model/Conexao.class.php");
 
-$RemediosDAOImple = new RemediosDAOImple();
+if(isset($_POST['submit'])){
+    $controller = new DeleteRemedioController();
+    $controller->DeletarRemedio($_POST);
+}
 
-$id = $_POST['id'];
 
-if (isset($id) && !empty($id)){
-    $RemediosDAOImple->deleteRemedio($_POST);
-    header("Location: .../view/DeletaRemedio.php?cod=1");
+class DeleteRemedioController{
+
+    private $remedioDAO;
+    public function __construct(){
+        $this->remedioDAO = new RemediosDAOImpl();
+    }
+    
+    public function DeletarRemedio($dados){
+
+       $remedio = new Remedio();
+       $remedio->Id = $dados['id'] ?? '';
+      
+
+       $this->remedioDAO->insertRemedios($remedio);
+
+       header("Location: ../../View/VerRemedio.php?cod=3");
+       exit();
+
+    }
 }
 
 ?>
