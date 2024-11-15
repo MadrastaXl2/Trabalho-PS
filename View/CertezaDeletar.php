dbb821eea218b1df4b9c5f4ff0cb73117ab0b775
podreiro <rafaelp3r3ira@gmail.com>
2024-11-15 17:39:05 -0300

    mudanças no href dos css

--- View/CertezaDeletar.php
+++ View/CertezaDeletar.php
@@ -10,7 +10,7 @@ include "../Model/ManagerUsuario.class.php";
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="../Trabalho-PS/View/CSS/Cabecalho.css">
+    <link rel="stylesheet" href="CSS/Cabecalho.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
